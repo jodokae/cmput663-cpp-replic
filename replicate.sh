fd48a9174152537a0652e4805d080341f2edd141
Johannes <sir_stormrider@web.de>
2017-10-29 17:01:56 -0600

    fixed

--- replicate.sh
+++ replicate.sh
@@ -11,19 +11,21 @@ AddVersion() {
     wget "https://github.com/apache/httpd/archive/$1.tar.gz"
     tar -xzf "$1.tar.gz"
     
-    mv "httpd-$1/*" "source/"
+    mv "httpd-$1"/* "source/"
     
-    #rm "$1.tar.gz"
-    #rm -r "httpd-$1"
+    rm "$1.tar.gz"
+    rm -r "httpd-$1"
     
     cd ../..
     echo "./projects/httpd-$1" >> cppstats_input.txt
 }
 
-sudo apt-get install python python-dev python-doc python-setuptools gcc g++ clang python-lxml astyle xsltproc boolstuff git subversion wget dpkg csvtool
+sudo apt-get install python python-dev python-doc python-setuptools gcc g++ clang python-lxml astyle xsltproc boolstuff git subversion wget dpkg #csvtool
 
 wget http://131.123.42.38/lmcrs/beta/srcML-Ubuntu14.04-64.deb
 sudo dpkg -i srcML-Ubuntu14.04-64.deb
+
+rm srcML-Ubuntu14.04-64.deb
 
 git clone https://github.com/clhunsen/cppstats
 cd cppstats
@@ -33,13 +35,13 @@ echo "" > cppstats_input.txt
 
 mkdir projects
 AddVersion "2.2.11"
-AddVersion "2.2.33"
-AddVersion "2.2.34"
+AddVersion "2.4.25"
+AddVersion "2.4.26"
 AddVersion "2.4.27"
 AddVersion "2.4.28"
 AddVersion "2.4.29"
 
-#cppstats --kind general
+cppstats --kind general
 
 #cd projects/httpd-2.2.11
 
